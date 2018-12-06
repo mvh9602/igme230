68a2656e30b3f45dcdd3be2966a65269e188dd1d
Matt Hoffman <mvh9602@g.rit.edu>
2018-12-06 13:40:41 -0500

    Finished practice practical

--- practice_practical/fall2018sample.js
+++ practice_practical/fall2018sample.js
@@ -1,1 +1,14 @@
 /* Don't use <script> tags in a linked js file! */
+
+$(".menuitem").click(function() {
+    $(this).next(".submenu").slideToggle("slow");
+});
+
+let display = "content1.txt";
+$("#choose-content").val(display);
+$("#content").load(display);
+
+$("#choose-content").change(function() {
+    display = $(this).val();
+    $("#content").load(display);
+})
