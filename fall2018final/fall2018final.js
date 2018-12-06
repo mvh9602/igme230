c7537cbddaaa2b5245035102aaefe74d506878c2
Matt Hoffman (RIT Student) <mvh9602@ad.rit.edu>
2018-12-06 18:02:44 -0500

    trying again

--- fall2018final/fall2018final.js
+++ fall2018final/fall2018final.js
@@ -8,7 +8,14 @@ let display = "article0.txt";
 $("form").val(display);
 $("article").load(display);
 
-$("form").change(function() {
+$("input[type=radio][name=article]").change(function() {
     display = $(this).val();
     $("article").load(display);
 });
+
+let count = 0;
+$("#count").html(count);
+$("#clickme").click(function() {
+    count++;
+    $("#count").html(count);
+})
